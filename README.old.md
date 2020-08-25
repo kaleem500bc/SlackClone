7299664adaef32a100f21dae19a14494c78f8df6
Kaleem500bc <kaleem500bc@gmail.com>
2020-08-25 18:11:07 +0200

    Setup project and Create Routes

---
+++ README.old.md
@@ -0,0 +1,12 @@
+# SlackClone
+
+
+
+#npx create-react-app "name of App" //create the app
+
+#npm i react-gitignore
+
+#npm i  //or npm install
+
+#npm start // start the app
+
---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+
+
+//todo
+const Root = () => (
+  <Router>
+    <Switch>
+      //todo 'exact'
+      <Route exact path="/" component={App}/>
+      <Route path="/login" component={Login}/>
+      <Route path = "/register" component={Register}/>
+    </Switch>
+  </Router>
+)
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
